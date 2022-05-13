3f3f855aef29bbe0ffa169aa5ba5325920cfd03a
olen-d <olen.daelhousen@outlook.com>
2022-05-12 23:56:16 -0400

    Add address of the starting point

---
+++ src/components/DisplayAddress.vue
@@ -0,0 +1,73 @@
+<script setup>
+import LocationCourseShowGoogleMaps from '@/components/LocationCourseShowGoogleMaps.vue'
+
+const props = defineProps({
+  icon: {
+    type: String,
+    default: "fa-solid fa-arrow-right",
+  },
+  label: {
+    type: String,
+    default: "label",
+  },
+  location: {
+    type: Object,
+    required: true
+  },
+  showCoordinates: {
+    type: Boolean,
+    default: true
+  },
+    showIcon: {
+    type: Boolean,
+    default: false
+  },
+    showLabel: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
+
+<template>
+  <div class="display-address">
+    <div class="heading" v-if="showIcon || showLabel">
+      <h3>
+        <i v-if="showIcon" :class="icon"></i>
+        <span v-if="showIcon && showLabel">&nbsp;</span>
+        <span class="label" v-if="showLabel">
+          {{ label }}
+        </span>
+      </h3>
+    </div>
+    {{ location.address }}<br />
+    {{ location.city }},&nbsp;{{ location.state }}&nbsp;{{ location.postcode }}
+    <div class="coordinates">
+      {{ location.latitude }}, {{ location.longitude }}
+    </div>
+    <LocationCourseShowGoogleMaps :latitude="location.latitude" :longitude ="location.longitude" />
+  </div>
+</template>
+
+<style scoped>
+.display-address {
+  font-size: 1.1rem;
+}
+
+.coordinates {
+  margin-top: 0.5rem;
+  margin-bottom: 0.5rem;
+}
+
+.heading h3 {
+  font-weight: 500;
+  text-transform: uppercase;
+  color: #ff6b25;
+}
+
+@media (min-width: 1024px) {
+  .heading h3 {
+    text-align: left;
+  }
+} 
+</style>
---
+++ src/components/LocationCourseShowGoogleMaps.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  latitude: {
+    type: Number,
+    required: true
+  },
+  longitude: {
+    type: Number,
+    required: true
+  }
+})
+
+const uri = ref('')
+
+uri.value= encodeURI(`https://www.google.com/maps/search/?api=1&query=${props.latitude},${props.longitude}`)
+</script>
+
+<template>
+  <a :href="uri">
+    <i class="fa-solid fa-map-location-dot"></i> Show on Google Maps
+  </a>
+</template>
+
+<style scoped>
+a {
+  padding: 0.3rem;
+}
+
+a:link {
+  color: #ff6b25;
+  text-decoration: none;
+}
+
+a:visited {
+  color: #ff6b25;
+}
+
+a:hover {
+  color: #fff;
+  background: rgba(255, 107, 37, 0.6);
+}
+</style>
